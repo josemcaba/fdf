b2c0d9eeb89727f8ac58de06feba86c40142e168
Jose M. Caballero <josemcaba@gmail.com>
2023-07-14 11:46:11 +0200

    ALgunoss cambios que se quedaron atras ;-)

--- fdf.c
+++ fdf.c
@@ -12,6 +12,18 @@
 
 #include "fdf.h"
 
+int	open_new_window(t_map *map)
+{
+	map->mlx = mlx_init(WIDTH, HEIGHT, \
+			"FdF by Jose M. Caballero", true);
+	if (!map->mlx)
+	{
+		ft_printf("Can not open a new window.\n");
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
+}
+
 int	init_map(char *fname, t_map *map)
 {
 	if (read_map_file(fname, &map) == EXIT_FAILURE)
@@ -22,6 +34,7 @@ int	init_map(char *fname, t_map *map)
 	map->img = NULL;
 	map->string = NULL;
 	set_dimensions(&map);
+	set_isometric(&map);
 	return (EXIT_SUCCESS);
 }
 
@@ -31,14 +44,13 @@ int	main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		ft_printf("Please enter just one FDF MAP file\n");
+		ft_printf("Please enter just one FDF MAP file.\n");
 		return (EXIT_FAILURE);
 	}
+	if (open_new_window(&map) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
 	if (init_map(argv[1], &map) != EXIT_SUCCESS)
 		return (EXIT_FAILURE);
-	map.mlx = mlx_init(WIDTH, HEIGHT, \
-			"FdF by Jose M. Caballero", true);
-	set_isometric(&map);
 	mlx_loop_hook(map.mlx, ft_draw, &map);
 	mlx_loop_hook(map.mlx, ft_hook, &map);
 	mlx_loop(map.mlx);
